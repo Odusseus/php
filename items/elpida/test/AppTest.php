224962aca6a62157cb03910ff880c3bd050e1e97
odusseus <info@odusseus.org>
2020-03-28 02:01:02 +0100

    Added unittest for App.php

---
+++ items/elpida/test/AppTest.php
@@ -0,0 +1,35 @@
+<?php
+
+include('/Githup/Odusseus/php/items/elpida/source/App.php'); // must include if tests are for non OOP code
+
+class AppTest extends PHPUnit\Framework\TestCase
+{
+
+  /** @test */
+  public function CheckTest_Shoul_Return_True_When_Exist()
+  {
+    // arrange  
+    $name = "test";
+
+    // act
+    $result = Elpida\App::check($name);
+
+    // assert
+    $this->assertTrue($result);
+  }
+
+  /** @test */
+  public function CheckTest_Shoul_Return_False_When_Not_Exist()
+  {
+    // arrange  
+    $name = "toto";
+
+    // act
+    $result = Elpida\App::check($name);
+
+    // assert
+    $this->assertFalse($result);
+  }
+}
+
+?>
