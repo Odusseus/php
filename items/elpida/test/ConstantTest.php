cc31ce16139909f1593b61bce7952d5cd82207cc
odusseus <info@odusseus.org>
2020-04-03 11:28:16 +0200

    Unittest added

---
+++ items/elpida/test/ConstantTest.php
@@ -0,0 +1,48 @@
+<?php namespace Elpida;
+use Elpida\Constant;
+use PHPUnit\Framework\TestCase;
+
+include '/Githup/Odusseus/php/items/elpida/source/Constant.php';
+
+class ConstantTest extends TestCase
+{
+  /**
+   * @test
+   * @dataProvider constantVariables
+   * 
+   */
+  public function define_Should_Return_Value($constant, $assert)
+  {
+    // arrange
+
+    // act
+    $result = $constant;
+
+    //assert
+    $this->assertEquals($result, $assert);
+  }
+
+  public function constantVariables()
+  {
+    return [
+      [ACTIVATION_CODE, "activationcode"],
+      [APPNAME, "appname"],
+      [COOKIE, "elpida"],
+      [EMAIL, "email"],
+      [IP, "ip"],      
+      [ISALIVE, "isalive"],
+      [JSON_DIR, "json"],
+      [MAIL_DIR, "mail"],
+      [MAX_CREATEUSER, "createuser"],
+      [NICKNAME, "nickname"],
+      [PASSWORD, "password"],
+      [STATE_TRUE, "true"],
+      [STATE_FALSE, "false"],
+      [USER_DIR, "user"],
+      [VALUE, "value"],
+      [VALUE_DIR, "value"],
+      [TXT_DIR, "txt"]
+    ];
+  }
+}
+?>
