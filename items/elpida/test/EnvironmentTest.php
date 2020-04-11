2c285b5ab68644272b643995cd8e341c48fd3094
odusseus <info@odusseus.org>
2020-04-11 21:59:38 +0200

    Unittest added

---
+++ items/elpida/test/EnvironmentTest.php
@@ -0,0 +1,33 @@
+<?php
+use Elpida\Environment;
+use PHPUnit\Framework\TestCase;
+
+include '/Githup/Odusseus/php/items/elpida/source/Environment.php';
+
+class EnvironmentTest extends TestCase
+{
+  /**
+   * @test
+   * @dataProvider constantVariables
+   * 
+   */
+  public function define_Should_Return_Value($constant, $assert)
+  {
+    // arrange
+
+    // act
+    $result = $constant;
+
+    //assert
+    $this->assertEquals($result, $assert);
+  }
+
+  public function constantVariables()
+  {
+    return [
+      [DEBUG, "true"],
+      [DATA_DIR, "datatest"]
+    ];
+  }
+}
+?>
