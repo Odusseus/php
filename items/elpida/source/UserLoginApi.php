25f677dab3536cd1ef9c06e9422f02eebe46f1b3
odusseus <info@odusseus.org>
2020-05-31 17:14:51 +0200

    Swagger attributes added for UserLoginApi.

--- items/elpida/source/UserLoginApi.php
+++ items/elpida/source/UserLoginApi.php
@@ -1,5 +1,62 @@
 <?php namespace Items;
 
+/**
+ * @OA\Get(
+ *     path="/php/elpida/UserLoginApi.php?isalive",
+ *     @OA\Response(
+ *       response="200",
+ *       description="When is alive.")
+ * )
+ */
+
+/**
+ * @OA\Post(
+ *     path="/php/elpida/UserLoginApi.php",
+ *     summary="User log in for a app and get a authorisation cookie.",
+ *     description="User log in for a app.",
+ *     @OA\RequestBody(
+ *         @OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(
+ *                 @OA\Property(
+ *                     property="appname",
+ *                     type="string"
+ *                 ),
+ *                 @OA\Property(
+ *                     property="nickname",
+ *                     type="string"
+ *                 ),
+ *                 @OA\Property(
+ *                     property="password",
+ *                     type="string"
+ *                 ),
+ *                 example={
+ *                   "appname": "myApp",
+ *                   "nickname": "myNickname",
+ *                   "password": "myPassword",
+*                 }
+ *             )
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response=200,
+ *         description="Success when user is log in. He gets a authorisation cookie"
+ *     ),
+ *     @OA\Response(
+ *         response=403,
+ *         description="FORBIDDEN when Ip is blacklisted or when User already exists.."
+ *     ),
+ *     @OA\Response(
+ *         response=404,
+ *         description="NOT_FOUND when APPNAME/NICKNAME/PASSWORD or the combination appname and nickname are not found."
+ *     ),
+ *     @OA\Response(
+ *         response=422,
+ *         description="UNPROCESSABLE_ENTITY when APPNAME/NICKNAME/PASSWORD is missing."
+ *     ),
+ * )
+ */
+
 require_once "enum/HttpCode.php";
 require_once "Constant.php";
 require_once "UserLoginLogic.php";
